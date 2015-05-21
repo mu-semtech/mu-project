24090f07768db9885b6dc4fed8c5708defe26cd8
Aad Versteden <madnificent@gmail.com>
2015-05-21 19:17:19 +0200

    Initial import
    This contains the code which was previously contained in mu-dispatcher.
    With this project, we have a cleaner basis to define the new projects
    using mu.semte.ch.

---
+++ config/dispatcher.ex
@@ -0,0 +1,41 @@
+defmodule Dispatcher do
+  use Plug.Router
+
+  def start(_argv) do
+    port = 80
+    IO.puts "Starting Plug with Cowboy on port #{port}"
+    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
+    :timer.sleep(:infinity)
+  end
+
+  plug Plug.Logger
+  plug :match
+  plug :dispatch
+
+  get "/hello" do
+    send_resp( conn, 200, "world" )
+  end
+
+  get "/world" do
+    send_resp( conn, 200, "42" )
+  end
+
+  get "/" do
+    send_resp( conn, 200, "This is plug" )
+  end
+
+  match "/session/*path" do
+    # eg: POST /session/logout
+    Proxy.forward conn, path, "http://login/"
+  end
+
+  match "/comments/*path" do
+    IO.puts "Matching /comments/*path"
+    Proxy.forward conn, path, "http://comments/"
+  end
+
+  match _ do
+    send_resp( conn, 404, "Route not found" )
+  end
+
+end
