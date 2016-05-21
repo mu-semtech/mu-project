c6c432e19a1439b6df6376dc00ac138e27cab320
Aad Versteden <madnificent@gmail.com>
2016-05-21 10:32:30 +0200

    Fix namespace of ext
    This was under /vocabulary/ext/, rather than /vocabularies/ext/.

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -31,4 +31,4 @@
 ;; the mu.semte.ch organisation indicates that it will not be used
 ;; by them and that it shouldn't be used for permanent URIs.
 
-(add-prefix "ext" "http://mu.semte.ch/vocabulary/ext/")
+(add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
