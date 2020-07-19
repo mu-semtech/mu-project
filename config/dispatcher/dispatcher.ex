3dd3c6397d930e494f4b34afbeffdc4c75eb4405
jonathan langens <flowofcontrol@gmail.com>
2020-07-20 00:15:37 +0200

    Updates default project to include new mu.semte.ch additions

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,29 +1,12 @@
 defmodule Dispatcher do
-  use Plug.Router
+  use Matcher
+  define_accept_types [
+    json: [ "application/json", "application/vnd.api+json" ]
+  ]
 
-  def start(_argv) do
-    port = 80
-    IO.puts "Starting Plug with Cowboy on port #{port}"
-    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
-    :timer.sleep(:infinity)
-  end
+  @json %{ accept: %{ json: true } }
 
-  plug Plug.Logger
-  plug :match
-  plug :dispatch
-
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
-  #
-  # docker-compose stop; docker-compose rm; docker-compose up
-  # after altering this file.
-  #
-  # match "/themes/*path" do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-
-  match _ do
+  match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
-
 end
