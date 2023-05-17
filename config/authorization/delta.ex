02c944c093ae2011bfdde99cf02363e528367bd5
Aad Versteden <madnificent@gmail.com>
2023-05-17 11:17:51 +0200

    Add authorization endpoint to default mu-project

---
+++ config/authorization/delta.ex
@@ -0,0 +1,5 @@
+defmodule Delta.Config do
+  def targets do
+    [ "http://delta-notifier" ]
+  end
+end
--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,11 @@
 (in-package :mu-cl-resources)
 
+(setf *verify-content-type-header* nil)
+(setf *verify-accept-header* nil)
+(setf *include-count-in-paginated-responses* t)
+(setf *supply-cache-headers-p* t)
+(setf sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+(setf *cache-model-properties-p* t)
+
 ;; reading in the domain.json
 (read-domain-file "domain.json")
