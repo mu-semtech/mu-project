aeb9795ed8c2f4d93962bece76b0fb04af4e6f0d
Erika Pauwels <erika.pauwels@gmail.com>
2023-09-29 17:10:18 +0200

    Sensible defaults for mu-cl-resources

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,9 @@
 (in-package :mu-cl-resources)
 
+(setf *include-count-in-paginated-responses* t)
+(setf *supply-cache-headers-p* t)
+(setf sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+(setf *cache-model-properties-p* t)
+
 ;; reading in the domain.json
 (read-domain-file "domain.json")
